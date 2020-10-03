ffc49e772abe5ef953b135fe347fa80bdcc8c74f
Сидоров Юра <Odessa202@gmail.com>
2020-10-03 23:54:45 +0300

    Added car manager

---
+++ Cars/Managers/CarManager.cs
@@ -0,0 +1,51 @@
+﻿using Cars.Adapters;
+using Cars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Managers
+{
+    public class CarManager : ICarManager
+    {
+        ICarAdapter _carAdapter;
+
+        public CarManager(ICarAdapter carAdapter) => _carAdapter = carAdapter;
+
+        public async Task<Car> Create(string name, string description)
+        {
+            string id;
+            Car car = await GetLast();
+            if (car == null)
+            {
+                id = "1";
+            }
+            else
+            {
+                int newId = int.Parse(car.Id);
+                id = $"{newId + 1}";
+            }
+            await _carAdapter.Create(id, name, description);
+            return await GetLast();
+        }
+
+        public async Task Delete(string id) => await _carAdapter.Delete(id);
+
+        public async Task<IEnumerable<Car>> Enumerate() => await _carAdapter.Enumerate();
+
+        public async Task<Car> Get(string id) => await _carAdapter.Get(id);
+
+        public async Task<Car> GetLast()
+        {
+            IEnumerable<Car> cars = await Enumerate();
+            return cars.LastOrDefault();
+        }
+
+        public async Task<Car> Update(string id, string name, string description)
+        {
+            await _carAdapter.Update(id, name, description);
+            return await _carAdapter.Get(id);
+        }
+    }
+}
---
+++ Cars/Managers/ICarManager.cs
@@ -0,0 +1,16 @@
+﻿using Cars.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cars.Managers
+{
+    public interface ICarManager
+    {
+        Task<Car> Create(string name, string description);
+        Task<Car> Update(string id, string name, string description);
+        Task Delete(string id);
+        Task<Car> Get(string id);
+        Task<Car> GetLast();
+        Task<IEnumerable<Car>> Enumerate();
+    }
+}
