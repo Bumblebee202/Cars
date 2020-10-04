6ec2e72cd244aa96578743b05398f4ae9e447cfc
Сидоров Юра <Odessa202@gmail.com>
2020-10-05 02:08:11 +0300

    added front-end

---
+++ Cars/ClientApp/src/app/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Car } from './car';
+
+@Injectable()
+export class DataService {
+
+  private url = "/api/cars";
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCars() {
+    return this.http.get(this.url);
+  }
+
+  getCar(id: string) {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  createCar(car: Car) {
+    return this.http.post(this.url, car);
+  }
+  updateCar(car: Car) {
+
+    return this.http.put(this.url, car);
+  }
+  deleteCar(id: string) {
+    return this.http.delete(this.url + '/' + id);
+  }
+}
