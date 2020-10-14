80ffb55a8351202fc68ae827e7c9cddcdc9ee52a
Сидоров Юра <Odessa202@gmail.com>
2020-10-15 02:34:10 +0300

    changed Model

--- Cars/Models/Car.cs
+++ Cars/Models/Car.cs
@@ -1,7 +1,11 @@
-﻿namespace Cars.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Cars.Models
 {
     public class Car
     {
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
